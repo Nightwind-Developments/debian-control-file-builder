e5699ecf91b2ae4d93f572d8f3892217276d9252
Rowan Rathod <rowanrathod@nightwind-developments.co.nz>
2020-12-26 12:50:43 +1300

    Improved Entrypoint File

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,33 +2,25 @@
 
 # The following script was referenced from: https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
 
-POSITIONAL=()
-while [[ $# -gt 0 ]]
+while [ "$1" != "" ]
 do
 key="$1"
-
+data="$2"
 case $key in
     -f|--config_file)
-    DEB_CONFIG_FILE="$2"
+    DEB_CONFIG_FILE="$data"
     shift # past argument
     shift # past value
     ;;
     -d|--deps_file)
-    DEB_DEPS_FILE="$2"
+    DEB_DEPS_FILE="$data"
     shift # past argument
     shift # past value
     ;;
-    --default)
-    DEFAULT=YES
-    shift # past argument
-    ;;
-    *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
-    ;;
+    *)
+      echo "ERROR: Invalid parameter key (\"$key\")"
 esac
 done
-set -- "${POSITIONAL[@]}" # restore positional parameters
 
 echo "CONFIGURATION FILE  = ${DEB_CONFIG_FILE}"
 echo "DEPENDENCY FILE     = ${DEB_DEPS_FILE}"
