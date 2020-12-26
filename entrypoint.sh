4b778dd49600e359e2f46c98dc167ee61990fb37
Rowan Rathod <rowanrathod@nightwind-developments.co.nz>
2020-12-26 15:08:34 +1300

    Implemented Generated File Path Action Variable
    Users can now get the full path of the generated control file as an Action output variable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # The following script was referenced from: https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
 # And here: https://gist.github.com/jehiah/855086
 
+# Parses the Argument Options
 while [ "$1" != "" ]
 do
 key="$1"
@@ -13,8 +14,13 @@ case $key in
     shift # past argument
     shift # past value
     ;;
-    -d|--deps_file)
+    -df|--deps_file)
     DEB_DEPS_FILE="$data"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--output)
+    DEB_CTRL_OUT="$data"
     shift # past argument
     shift # past value
     ;;
@@ -25,16 +31,22 @@ case $key in
 esac
 done
 
+# Lists the Arguments that were found
 echo "\nArguments Read from Entrypoint Script:"
 echo "CONFIGURATION FILE  = ${DEB_CONFIG_FILE}"
 echo "DEPENDENCY FILE     = ${DEB_DEPS_FILE}"
-
-echo "\nCurrent Runtime Directory:"
-ls -la .
+echo "CONTROL FILE OUTPUT = ${DEB_CTRL_OUT}"
 
 echo "Running Debian Control File Builder"
-if dcb -f ${DEB_CONFIG_FILE} -df ${DEB_DEPS_FILE} ; then
-  echo "Running App Succeeded"
+DCB_ARGS="-f ${DEB_CONFIG_FILE} -df ${DEB_DEPS_FILE}"
+# Appends output path to arguments variable if one is provided
+if ${DEB_CTRL_OUT} ; then
+  DCB_ARGS="${DCB_ARGS} -o ${DEB_CTRL_OUT}"
+fi
+# Runs the Debian Control File Generator, Reports if it was Successful
+if dcb ${DCB_ARGS} ; then
+  echo "Success"
+  echo "::set-output name=output-path::${DEB_CTRL_OUT}/control"
 else
-  echo "Running App Failed"
+  echo "App Failed"
 fi
